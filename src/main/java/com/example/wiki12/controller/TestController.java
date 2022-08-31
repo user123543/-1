4ec7536b94465b026dc1e5d30f94f0c048fcd2b9
docker <lucilly@163.com>
2022-09-01 00:15:15 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/example/wiki12/controller/TestController.java
+++ src/main/java/com/example/wiki12/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki12.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,9 @@ public class TestController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         return "hello world";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 }
