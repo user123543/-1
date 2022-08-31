ab25f2cf12cf4983e950d59ade166a2ab7f895fa
docker <lucilly@163.com>
2022-08-31 22:05:07 +0800

    2-2 init

---
+++ src/main/java/com/example/wiki12/Wiki12Application.java
@@ -0,0 +1,13 @@
+package com.example.wiki12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wiki12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wiki12Application.class, args);
+    }
+
+}
