4b139959796fe9c3b6d66fdcfcd2592fb90bd978
docker <lucilly@163.com>
2022-08-31 23:50:04 +0800

    2-6 开发hello world接口 访问地址:http://127.0.0.1:8880/hello

---
+++ src/main/java/com/example/wiki12/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.wiki12.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello world";
+    }
+}
